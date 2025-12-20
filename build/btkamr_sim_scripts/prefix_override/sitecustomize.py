af96769f3d60d7e9e8df8d71030aa4b92cd2a3d7
Mustafa Eren BAŞOL <merenbasol@gmail.com>
2025-12-20 20:49:54 +0300

    .

--- build/btkamr_sim_scripts/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/meren/Projects/btkamr_edu/install/btkamr_sim_scripts'
--- build/btkamr_simulate/CMakeFiles/btkamr_simulate_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/btkamr_simulate_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/btkamr_simulate_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
